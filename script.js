235f6886866c5e0e74a04397c0a5d9c123ed2127
Brandon Winger-Air <brandonwinger-air@outlook.com>
2025-05-13 01:08:50 -0400

    setup submenu structure, functionality and styles

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// Menu Dropdown
+document.querySelectorAll('.menu-item').forEach(item => {
+    item.addEventListener('mouseenter', () => {
+        item.querySelector('.submenu').style.display = 'block';
+    });
+
+    item.addEventListener('mouseleave', () => {
+        item.querySelector('.submenu').style.display = 'none';
+    });
+});
+
+// Tuner
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 const analyser = audioCtx.createAnalyser();
 
